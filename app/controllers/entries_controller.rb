b31b2757b5233c74e2b01253ecf645a77f5a55f3
Kwin Palanuwech <kwin.palanuwech@kellogg.northwestern.edu>
2024-05-14 22:00:50 +0000

    almost done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,33 @@
 class EntriesController < ApplicationController
+  def show
+    @entry = Entry.find_by({"id" => params["id"]})
+    @place= Place.find_by({"id" => @entry["place_id"]})
+    # render entry/show view with details about entry
+  end
+
+  def new
+    @place = Place.find_by({ "id" => params["place_id"] })
+    # render entry/new view with new entry form
+  end
+
+  def create
+    # start with a new entry
+    @entry = Entry.new
+
+    # assign user-entered form data to entry's columns
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["posted_on"]
+
+    # assign relationship between entry and place
+    @entry["place_id"] = params["place_id"]
+
+    # save entry row
+    @entry.save
+
+    # redirect user
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
+
 end
+
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,31 @@
 class PlacesController < ApplicationController
-
   def index
-    render :template => "places/index"
+    @places = Place.all
+  end
+    
+  def show
+    # find a place
+    @place = Place.find_by({ "id" => params["id"] })
+    # find entries for the place
+    @entries = Entry.where({ "place_id" => @place["id"] })
+    # render place/show view with details about Company
+  end
+
+  def new
+    # render view with new entry form
+  end
+
+  def create
+    # start with a new entry
+    @place = Place.new
+
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["name"]
+
+    # save place row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
   end
 end
